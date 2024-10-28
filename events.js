4ef26fcc735c8e95f1150cc1753fb1690c4f6e50
Ashar Huda <hudaau@uci.edu>
2024-10-28 00:45:52 -0700

    i think i finally finished the typewriter animations it took a me a very long time

--- events.js
+++ events.js
@@ -1,9 +1,8 @@
 document.getElementById("social-links").addEventListener("click", function(event) {
     const link = event.target;
-
     if (link.classList.contains('fa-linkedin')) {
         window.open('https://www.linkedin.com/in/asharhuda/', '_blank');
     } else if (link.classList.contains('fa-github')) {
-        window.open('https://github.com/asharhuda', '_blank');
+        window.open('https://github.com/ahuda07', '_blank');
     }
 });
